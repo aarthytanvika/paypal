5f63a6d222e57eb794aae0304bc0888fe134262b
aarthytanvika <aarthytanvika2812@gmail.com>
2023-05-09 15:43:36 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+import{ render } from 'creditcardpayments/creditCardPayments';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'upi';
-}
+  
+  constructor(){
+   
+  render( 
+    {
+      id: "#paypal-button-container",
+      value: "100.00",
+      currency: "USD",
+      onApprove: (details) =>{
+        alert("Transaction Successfull");
+      }
+    }
+  );
+      
+  }
+  title='upi';
+  }
+  
+  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaypalButtonComponent } from './paypal-button/paypal-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaypalButtonComponent
   ],
   imports: [
     BrowserModule,
